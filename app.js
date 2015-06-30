f4c00a43b098a76acd080db3b847d255949d0351
gmiriam <gmiriam87@gmail.com>
2015-06-30 20:51:08 +0100

    Arreglo del código para el funcionamiento con express v4

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-var router = express.Router();
+/*var router = express.Router();
 
 router.get('/', function(req, res) {
    res.send("Hello World!");
 });
 
-app.use(router);
+app.use(router);*/
 
 mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
     if (err)
@@ -23,7 +23,24 @@ mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
         console.log('Conectado a la base de datos');
 });
 
-routes = require('./routes/tvshows')(app);
+//routes = require('./routes/tvshows')(app);
+var model = require('./models/tvshow')(app, mongoose);
+
+var TVShowCtrl = require('./controllers/tvshows');
+
+// API routes
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
 
 app.listen(3000, function() {
   console.log("Node server running on http://localhost:3000");
