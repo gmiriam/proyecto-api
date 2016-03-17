007affb9543cc8677f1a331bbc1c01c0a237a330
gmiriam <gmiriam87@gmail.com>
2016-03-17 12:16:00 +0000

    Intento de plugin

--- app.js
+++ app.js
@@ -37,50 +37,9 @@ var adminModel = require('./models/admin');
 var mongoose = require('mongoose');
 var Admin = mongoose.model('admin');
 
-seneca.add('role:api,category:admin,cmd:findAll', function(args,done){
-    Admin.find(function(err, admins) {
-        if(!err) {
-            done(null,admins);
-        } else {
-            console.log('ERROR: ' + err);
-            done(err,'ERROR: ' + err);
-        }
-    });
-})
+require('seneca')()
+  .use(minimal_plugin, {foo: 'bar'})
 
-seneca.add('role:api,category:admin,cmd:findById', function(args,done){
-  Admin.findById(args._id, function(err, admin) {
-      if(!err) {
-        done(null, [admin]);
-      } else {
-        console.log('ERROR: ' + err);
-        done(null,{});
-      }
-    });
-})
-
-seneca.add('role:api,category:admin,cmd:add', function(args,done){
-  console.log('POST');
-
-  var obj = {
-    firstName:    args.firstName,
-    surname:     args.surname,
-    email:  args.email,
-    password:   args.password
-  };
-  var admin = new Admin(obj);
-  console.log(admin);
-
-  admin.save(function(err) {
-    if(!err) {
-      done(null,[admin]);
-      console.log('Created');
-    } else {
-      console.log('ERROR: ' + err);
-      done(err,'ERROR: ' + err);
-    }
-  });
-})
 
 seneca.act('role:web',{use:{
 
@@ -95,7 +54,9 @@ seneca.act('role:web',{use:{
   map:{
     findAll: {GET:true},          // explicitly accepting GETs
     findById: {GET: true, suffix: '/:_id'},
-    add: {POST: true}
+    add: {POST: true},
+	update: {PUT: true, suffix: '/:_id'},
+	delete: {DELETE: true, suffix: '/:_id'}
   }
 }})
 
