0b12bb64ae22983af707ff4657b194f87f017450
gmiriam <gmiriam87@gmail.com>
2016-12-15 20:10:37 +0000

    Amplia con pruebas de fichero y autenticación
    Añade ruta para recibir ficheros.
    Añade 'multer' para procesar subidas de ficheros.
    Añade 'oauth2-server' para exigir autenticación en rutas protegidas.
    También añade gestión de credenciales basada en el ejemplo
    'https://github.com/pedroetb/node-oauth2-server-mongo-example'.

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ var express = require('express'),
     mongoose = require('mongoose'),
     querystring = require('querystring'),
     http = require('http'),
-    cors = require('cors');
+    //cors = require('cors'),
+    oauthserver = require('oauth2-server');
 
+var port = 3002;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
-app.use(cors());
+//app.use(cors());
 
 mongoose.connect('mongodb://localhost/users', function(err, res) {
     if (err)
@@ -22,12 +24,24 @@ mongoose.connect('mongodb://localhost/users', function(err, res) {
         console.log('Conectado a la base de datos');
 });
 
+
+//*******************************************************
+
+app.oauth = oauthserver({
+  model: require('./model.js'),
+  grants: ['password']
+});
+
+app.use(app.oauth.errorHandler());
+
+app.all('/oauth/token', app.oauth.grant());
+
 //*******************************************************
 
 app.get('/login', function (req,res){
   var postOptions = {
     host: 'localhost',
-    port: 3001,
+    port: port,
     path: '/oauth/token',
     method: 'POST',
     headers: {
@@ -36,41 +50,45 @@ app.get('/login', function (req,res){
     }
 
   };
+
   var userName = req.query.usr,
     password = req.query.pwd;
 
   var usrDecoded = new Buffer(userName.toString(), 'base64').toString('ascii'),
-    pwdDecoded = new Buffer(password.toString(), 'base64').toString('ascii')
+    pwdDecoded = new Buffer(password.toString(), 'base64').toString('ascii');
+
   var postReq = http.request(postOptions, function(postRes) {
-      postRes.setEncoding('utf8');
-      postRes.on('data', function (chunk) {
-        var accessToken = JSON.parse(chunk).access_token;
-
-        var getOptions = {
-          host: 'localhost',
-          port: 3001,
-          path: '/',
-          method: 'GET',
-          headers: {
-              'Authorization': 'Bearer ' + accessToken
-          }
-        };
-        var getReq = http.request(getOptions, function(getRes) {
-          getRes.setEncoding('utf8');
-          getRes.on('data', function (chunk) {
-            res.send(chunk);
-          })
-        })
-        getReq.end();
-      });
+    postRes.setEncoding('utf8');
+    postRes.on('data', function (chunk) {
+      console.log("dentro del post", chunk);
+      var accessToken = JSON.parse(chunk).access_token;
+      res.send(chunk);
+    });
   });
-  postReq.write('grant_type=password&username='+usrDecoded +'&password=' +  pwdDecoded);
+  postReq.write('grant_type=password&username=' + usrDecoded + '&password=' + pwdDecoded);
   postReq.end();
+});
 
+function checkUserMiddleware(req, res, next) {
 
-})
+  var authorization = req.get("Authorization"),
+    token = authorization.split(" ").pop(),
+    tokenModel = require("./mongo/model/token");
+  console.log("me vino", token);
+  tokenModel.findOne({
+    accessToken: token
+  }, function(err, tokenFound) {
+    var user = tokenFound.user.username;
+    console.log("el token era de ", user);
+    if (user === "pedroetb") {
+      res.end("eres pedroetb, no te dejo pasar");
+    } else {
+      next();
+    }
+  })
+}
 
-app.get('/test', function (req,res) {
+app.get('/test', app.oauth.authorise(), checkUserMiddleware, function (req,res) {
 
   var exec = require('child_process').exec;
     cmd = "node_modules\\.bin\\intern-client",
@@ -83,6 +101,46 @@ app.get('/test', function (req,res) {
 
   exec(cmd + " " + args, cbk);
 });
+
+//*******************************************************
+
+var multer = require('multer');
+
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function (req, file, cb) {
+        cb(null, './data/');
+    },
+    filename: function (req, file, cb) {
+        var datetimestamp = Date.now();
+        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1]);
+    }
+});
+
+var upload = multer({ //multer settings
+  storage: storage
+}).single('file');
+
+app.use(function(req, res, next) { //allow cross origin requests
+        //res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+        //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Credentials", true);
+        next();
+    });
+app.post('/upload', function(req, res) {
+  upload(req,res,function(err){
+    var file = req.file;
+      console.log("fichero", file);
+      if(err){
+           res.json({error_code:1,err_desc:err});
+           return;
+      }
+      var filename = file.filename;
+      res.json({error_code:0,err_desc:null,filename:filename});
+  });
+});
+
+//*******************************************************
 
 seneca.use("plugins/admin", {})
 seneca.use("plugins/course", {})
@@ -99,7 +157,6 @@ app.use( seneca.export('web'))
 
 //*************************************************************
 
-var port = 3000;
 app.listen(port, function() {
   console.log("Node server running on http://localhost:" + port);
 });
